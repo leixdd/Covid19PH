aaf2c7a79d6cf9abecc76f62a0475fb227043a78
Lei <lecias.ariel@gmail.com>
2020-04-09 01:36:26 +0800

    added vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,98 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <v-app-bar app clipped-left>
+      <v-toolbar-title>Covid19 PH</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container fluid>
+        <v-row>
+          <v-col cols="12" md="8">
+            <v-row>
+              <v-col cols="12" md="6">
+                <v-card :color="item.color" dark>
+                  <div class="d-flex flex-no-wrap justify-space-between">
+                    <div>
+                      <v-card-title class="headline" v-text="item.title"></v-card-title>
+
+                      <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+                    </div>
+
+                    <v-avatar class="ma-3" size="125" tile>
+                      <v-img :src="item.src"></v-img>
+                    </v-avatar>
+                  </div>
+                </v-card>
+              </v-col>
+
+              <v-col cols="12" md="6">
+                <v-card dark>
+                  <div class="d-flex flex-no-wrap justify-space-between">
+                    <div>
+                      <v-card-title class="headline" v-text="item.title"></v-card-title>
+
+                      <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+                    </div>
+
+                    <v-avatar class="ma-3" size="125" tile>
+                      <v-img :src="item.src"></v-img>
+                    </v-avatar>
+                  </div>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-col>
+
+          <v-col cols="12" md="4">test</v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  props: {
+    source: String
+  },
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  data: () => ({
+    drawer: null,
+    ph_stats: {
+      updated: 1586367093695,
+      cases: 3870,
+      todayCases: 106,
+      deaths: 182,
+      todayDeaths: 5,
+      recovered: 96,
+      active: 3592,
+      critical: 1,
+      casesPerOneMillion: 35,
+      deathsPerOneMillion: 2,
+      tests: 24500,
+      testsPerOneMillion: 224
+    },
+    item: {
+      src: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
+      title: "Supermodel",
+      artist: "Foster the People"
+    }
+  }),
+
+  methods: {
+    async loadSource() {
+      // this.ph_stats = await axios.get().catch(err => console.log(err)).then(data => { return data;})
+    }
+  },
+
+  created() {
+    this.$vuetify.theme.dark = true;
+  }
+};
+</script>
