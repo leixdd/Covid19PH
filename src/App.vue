21b138aef09453aba14f32a37d42d23176cdf243
Lei <lecias.ariel@gmail.com>
2020-04-09 03:16:34 +0800

    applied covid api from lmao.ninja

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,8 @@
         <v-row>
           <v-col cols="12" md="8">
             <v-row>
-              <v-col 
-              v-for="(ph_stat, index) in ph_stats"
-              :key="index"
-              cols="12" md="6">
-                <v-card dark shaped="true">
+              <v-col v-for="(ph_stat, index) in ph_stats" :key="index" cols="12" md="6">
+                <v-card dark :shaped="true">
                   <div class="d-flex flex-no-wrap justify-space-between">
                     <div>
                       <v-card-title class="display-3 font-weight-black" v-text="ph_stat.data"></v-card-title>
@@ -42,7 +39,7 @@
 </template>
 
 <script>
-// import axios from "axios";
+import axios from "axios";
 
 export default {
   props: {
@@ -51,64 +48,58 @@ export default {
 
   data: () => ({
     drawer: null,
-    ph_stats : [
-      {
-        title : `Today's COVID-19 Cases`,
-        data: 106,
-        src: "mdi mdi-account-alert display-4",
-      },
-      {
-        title : `Total COVID-19 Cases in the Philippines`,
-        data: 3870,
-        src: "mdi mdi-account-group display-4",
-      },
-      {
-        title : `Number Active Patients`,
-        data: 3592,
-        src: "mdi mdi-account-multiple-plus display-4",
-      },
-      {
-        title : `Total Number of Recovered Patients`,
-        data: 96,
-        src: "mdi mdi-account-heart display-4",
-      },
-
-      {
-        title : `Today's Deaths`,
-        data: 5,
-        src: "mdi mdi-shield-cross display-4",
-      },
-      {
-        title : `Total Number of Deaths`,
-        data: 182,
-        src: "mdi mdi-grave-stone display-4",
-      },
-    ],
-    ph_: {
-
-      updated: 1586367093695,
-      cases: 3870,
-      todayCases: 106,
-      deaths: 182,
-      todayDeaths: 5,
-      recovered: 96,
-      active: 3592,
-      critical: 1,
-      casesPerOneMillion: 35,
-      deathsPerOneMillion: 2,
-      tests: 24500,
-      testsPerOneMillion: 224
-    }
+    ph_stats: []
   }),
 
   methods: {
     async loadSource() {
-      // this.ph_stats = await axios.get().catch(err => console.log(err)).then(data => { return data;})
+      this.ph_stats = await axios
+        .get("https://corona.lmao.ninja/countries/ph")
+        .catch(err => console.log(err))
+        .then(data => {
+          return [
+            {
+              title: `Today's COVID-19 Cases`,
+              data: data.data.todayCases,
+              src: "mdi mdi-account-alert display-4"
+            },
+            {
+              title: `Total COVID-19 Cases in the Philippines`,
+              data: data.data.cases,
+              src: "mdi mdi-account-group display-4"
+            },
+            {
+              title: `Number Active Patients`,
+              data: data.data.active,
+              src: "mdi mdi-account-multiple-plus display-4"
+            },
+            {
+              title: `Total Number of Recovered Patients`,
+              data: data.data.recovered,
+              src: "mdi mdi-account-heart display-4"
+            },
+
+            {
+              title: `Today's Deaths`,
+              data: data.data.todayDeaths,
+              src: "mdi mdi-shield-cross display-4"
+            },
+            {
+              title: `Total Number of Deaths`,
+              data: data.data.deaths,
+              src: "mdi mdi-grave-stone display-4"
+            }
+          ];
+        });
     }
   },
 
   created() {
     this.$vuetify.theme.dark = true;
+  },
+
+  mounted() {
+    this.loadSource();
   }
 };
 </script>
